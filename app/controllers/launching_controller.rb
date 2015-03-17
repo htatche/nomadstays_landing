c8f578ed026ebbf9d728f6723eb90015e2a933c6
Herve Tatche <herve.tatche@gmail.com>
2015-03-17 18:38:36 -0400

    Moved from static to a Rails server

---
+++ app/controllers/launching_controller.rb
@@ -0,0 +1,6 @@
+class LaunchingController < ApplicationController
+
+  def index
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NomadstaysLanding::Application.config.secret_key_base = '6e8f83811290face858fc3aeb5efb1f0aabe3ac6fa8c1d096ad7f832b23e31d8dbc3092bea0fdf19211f052aeaf6ba65e1cd7d97b4a560b792c0aec1d1fe5fea'
